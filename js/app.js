d57b0ab3ab828b1ba2088d2af264f6705d72cd74
Bryce Kolton <bbkolton@uw.edu>
2015-11-10 09:41:54 -0800

    lololol
    hehehehehheheheheheh

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 // Create app
-
+var app = angular.module('app', ['ui.router']);
 // Configure app
+app.config(function($stateProvider) {
+	$stateProvider.state('home', {
+		url: '/',
+		templateUrl: 'templates/home.html',
+		controller: 'homeCon'
+	}).state('about', {
+		url: '/about',
+		templateUrl: 'templates/about.html',
+		controller: 'aboutCon'
+	}).state('content', {
+		url: '/content',
+		templateUrl: 'templates/content.html',
+		controller: 'contentCon'
+	})
+
+
+});
 
 // Landing page controller: define $scope.number as a number
-
+app.controller('homeCon', function($scope) {
+	$scope.home = 'hometesterino';
+}).controller('aboutCon', function($scope) {
+	$scope.about = 'about testerinossss';
+}).controller('contentCon', function($scope) {
+	$scope.content = 'http://i.imgur.com/oS47R9e.gif';
+});
 
 // About page controller: define $scope.about as a string
 
